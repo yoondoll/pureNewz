96365c148856ed6f9ae886585f632cbda8d4f300
yoondoll <haidi1618@gmail.com>
2022-04-21 23:33:22 +0900

    Update

--- js/core.js
+++ js/core.js
@@ -18,6 +18,19 @@ $(document).ready(function(){
     tabUI(siTabBtn, siTabPage);
     tabUI(niTabBtn, niTabPage);
     tabUI(cosTabBtn, cosTabPage);
+
+    $(window).scroll(function(){
+        if($(this).scrollTop()>200){
+            $('.top').fadeIn();
+        } else{
+            $('.top').fadeOut();
+        }
+    });
+
+    $('.top').click(function(){
+        $('html, body').animate({scrollTop:0}, 400);
+        return false;
+    });
 });
 
 function init(){
